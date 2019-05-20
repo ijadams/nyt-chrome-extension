b7350a4cd3cb266576a3b9f9f25d78a77eff864c
Ian Adams <ijadams503@gmail.com>
2019-05-20 17:49:29 -0500

    initalize repo

---
+++ README.md
@@ -0,0 +1,5 @@
+
+NYT COOKING PAYWALL EXTENSION
+=======
+
+Chrome extension for getting around nyt cooking paywall
---
+++ popup.js
@@ -0,0 +1,3 @@
+$('.nytc---modal-window---contentBase').css('display', 'none');
+$('.nytc---shared---blackBG').css('display', 'none');
+$('.nytc---modal-window---noScroll').css('overflow', 'scroll');
