fa96d399a52af54627cee8f08700d5ee53eaeb61
Ian Adams <ian.adams@ge.com>
2019-06-23 15:42:32 -0500

    run at idle

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 setTimeout(() => {
+  console.log('ian');
 $('.nytc---modal-window---contentBase').css('display', 'none');
 $('.nytc---shared---blackBG').css('display', 'none');
 $('.nytc---modal-window---noScroll').css('overflow', 'scroll');
-}, 100);
-
+}, 0);
